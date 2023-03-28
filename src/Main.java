34bd8823baff26d51474afc3abbabf751df34bba
Saul Arteaga <alojera15@gmail.com>
2023-03-28 10:52:46 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Jejej");
+        final Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe un numero: ");
+        int numero = teclado.nextInt();
+        System.out.println("Tu numero es: \n" + numero);
     }
 }
